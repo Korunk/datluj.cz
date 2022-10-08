4c8752c47eb095258cb783dfc592508c983c982d
Klára Chmelařová <korunka91@gmail.com>
2022-10-08 23:26:33 +0200

    nové slovo

--- src/components/Wordbox/index.jsx
+++ src/components/Wordbox/index.jsx
@@ -1,8 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
-const Wordbox = ({ word }) => {
+const Wordbox = ({ word, onFinish }) => {
   const [lettersLeft, setLettersLeft] = useState(word);  
+  
+
+  const handleKeyUp = (e) => {
+   if (e.key.toLowerCase() === lettersLeft.toLowerCase().slice(e, 1)) {
+   setLettersLeft(lettersLeft.slice(1));
+  console.log(lettersLeft) }
+  
+  else if(lettersLeft.toLowerCase() === " " && e.key.toLowerCase() === lettersLeft.toLowerCase() ){
+    console.log("další slovo")
+   onFinish()}
+
+  else {
+  console.log("Písmenko není stejné!")
+}
+
+  }
+
+  useEffect(()=> {
+    document.addEventListener('keyup', handleKeyUp);
+    return () => document.removeEventListener('keyup',handleKeyUp);
+  }, [lettersLeft]);
+  
   
   return (
     <div className="wordbox">{lettersLeft}</div>
